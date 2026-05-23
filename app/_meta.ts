d7abcae5b3ed59b09f482cbfa3163b6ca7dcf425
roberto <tkp1@hanmail.net>
2026-05-23 10:41:07 +0900

    docs: 샘플 문서 페이지 3종 + 사이드바 메타 설정

---
+++ app/_meta.ts
@@ -0,0 +1,12 @@
+// 루트(navbar) 메뉴 순서.
+export default {
+  index: {
+    title: '홈',
+    type: 'page',
+    display: 'hidden',
+  },
+  docs: {
+    title: '문서',
+    type: 'page',
+  },
+}
---
+++ app/docs/_meta.ts
@@ -0,0 +1,7 @@
+// Nextra 4: 사이드바 순서와 라벨을 정의.
+// 키 = 폴더/파일명, 값 = 표시될 제목.
+export default {
+  'getting-started': '시작하기',
+  installation: '설치',
+  api: 'API 레퍼런스',
+}
